db60f94cffaba2877865d34a0928077a6749393a
ted.fernandes <ted.fernandess@gmail.com>
2016-08-23 09:54:56 +0200

    Removendo o construtor default com visibilidade do tipo protected que nao chega a ser usado - EspetaculosController

--- src/main/java/br/com/caelum/agiletickets/controllers/EspetaculosController.java
+++ src/main/java/br/com/caelum/agiletickets/controllers/EspetaculosController.java
@@ -38,10 +38,6 @@ public class EspetaculosController {
 	private Agenda agenda;
 	private DiretorioDeEstabelecimentos estabelecimentos;
 	
-	/** @deprecated CDI eyes only*/
-	protected EspetaculosController() {
-	}
-
 	@Inject
 	public EspetaculosController(Result result, Validator validator, Agenda agenda, DiretorioDeEstabelecimentos estabelecimentos) {
 		this.result = result;
